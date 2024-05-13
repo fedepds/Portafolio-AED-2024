96f68dee36a7203ced353e20988c703b8491c85b
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-05-13 19:33:50 -0300

    Ahora si
    jeje

--- Parcial1 Pizarro/src/main/java/uy/edu/ucu/aed/TArbolDeProductos.java
+++ Parcial1 Pizarro/src/main/java/uy/edu/ucu/aed/TArbolDeProductos.java
@@ -2,15 +2,27 @@ package uy.edu.ucu.aed;
 
 public class TArbolDeProductos extends TArbolBB<Producto> {
 
-/*    Como una medida posible que da una idea sobre la forma de un árbol binario, se define como "longitud de
-    trayectoria interna" (LTI) de un árbol a la suma de los niveles de todos los nodos del árbol, y como
-            "longitud de trayectoria interna media" (LTIM) a ese valor dividido por la cantidad de nodos del árbol
-            (tamaño longitudTrayectoriaInterna() debe calcular la suma de los niveles de todos los nodos del árbol */
+    /* Como una medida posible que da una idea sobre la forma de un árbol binario, se define como "longitud de
+       trayectoria interna" (LTI) de un árbol a la suma de los niveles de todos los nodos del árbol, y como
+       "longitud de trayectoria interna media" (LTIM) a ese valor dividido por la cantidad de nodos del árbol
+       (tamaño longitudTrayectoriaInterna() debe calcular la suma de los niveles de todos los nodos del árbol) */
 
+    /**
+     * Calcula la longitud de la trayectoria interna del árbol.
+     *
+     * @return La longitud de la trayectoria interna del árbol.
+     */
     public int longitudTrayectoriaInterna() {
         return longitudTrayectoriaInternaAux(this.raiz, 0);
     }
 
+    /**
+     * Método auxiliar para calcular la longitud de la trayectoria interna del árbol.
+     *
+     * @param nodo El nodo actual.
+     * @param nivel El nivel actual.
+     * @return La longitud de la trayectoria interna del árbol.
+     */
     public int longitudTrayectoriaInternaAux(TElementoAB<Producto> nodo, int nivel) {
         if (nodo == null) {
             return 0;
@@ -18,6 +30,12 @@ public class TArbolDeProductos extends TArbolBB<Producto> {
             return nivel + longitudTrayectoriaInternaAux(nodo.getHijoIzq(), nivel + 1) + longitudTrayectoriaInternaAux(nodo.getHijoDer(), nivel + 1);
         }
     }
+
+    /**
+     * Calcula la longitud media de la trayectoria interna del árbol.
+     *
+     * @return La longitud media de la trayectoria interna del árbol.
+     */
     public double longitudTrayectoriaInternaMedia() {
         if (this.raiz == null) {
             return 0;
@@ -25,9 +43,5 @@ public class TArbolDeProductos extends TArbolBB<Producto> {
             return (double) longitudTrayectoriaInterna() / this.raiz.obtenerTamaño();
         }
     }
-
-
-
-
-    
 }
+
