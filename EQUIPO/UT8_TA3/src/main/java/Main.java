65d9e453bdeeeaa8da47259710b26ea839603275
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-06-10 20:52:28 -0300

    arreglar

--- EQUIPO/UT8_TA3/src/main/java/Main.java
+++ EQUIPO/UT8_TA3/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        TGrafoNoDirigido grafo = UtilGrafos.cargarGrafo("/Users/federicopizarro/Desktop/UT8_TA3/src/main/java/verticesBEA.txt", "/Users/federicopizarro/Desktop/UT8_TA3/src/main/java/aristasBEA.txt",
+        TGrafoNoDirigido grafo = UtilGrafos.cargarGrafo("src/main/java/verticesBEA.txt", "src/main/java/aristasBEA.txt",
                 false, TGrafoNoDirigido.class);
         System.out.println("Grafo cargado en memoria");
 
